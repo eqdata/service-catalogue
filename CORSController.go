e00208dd00c771e7a49d3368d68f8b70b7d9505f
Alexander Sims <alexander.sims92@gmail.com>
2017-01-18 05:37:01 +0000

    Amended headers

--- CORSController.go
+++ CORSController.go
@@ -9,9 +9,9 @@ type CORSController struct { Controller }
 // Stores auction data to the Amazon RDS storage once it has been parsed
 func (c *CORSController) reply(w http.ResponseWriter, r  *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers",
-		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With, Access-Control-Request-Headers, Access-Control-Request-Method")
 
-	return
+	w.WriteHeader(http.StatusOK)
 }
